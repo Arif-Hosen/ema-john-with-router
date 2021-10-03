00be6f00966b163123ae0ada5a1eb52614666d8f
Arif-Hosen <aharifhosen1998@gmail.com>
2021-10-03 21:27:50 +0600

    inventory-added

--- src/components/Inventory/Inventery.js
+++ src/components/Inventory/Inventery.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventery = () => {
     return (
         <div>
-            <h2>This is Inventory.</h2>
+            <h2>This is Inventory added.</h2>
         </div>
     );
 };
